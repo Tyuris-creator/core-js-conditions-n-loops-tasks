3e28b87e43b2f9a770a3642d0831e6c1eb166c0b
Mikhail <tyuris@icloud.com>
2025-10-18 22:28:38 +0300

    feat:solved two things

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,15 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let max = 0;
+  const arr = [a, b, c];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
 }
 
 /**
